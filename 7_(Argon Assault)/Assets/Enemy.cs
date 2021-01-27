8d41b49b7932e38608d3f977321fe585935e3468
Cyberpixelgames <cyberpixelgames100@outlook.com>
2021-01-27 15:53:42 -0500

    Bullet Collisions and better Enemy Colliders

---
+++ 7_(Argon Assault)/Assets/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        Destroy(gameObject);
+    }
+}
