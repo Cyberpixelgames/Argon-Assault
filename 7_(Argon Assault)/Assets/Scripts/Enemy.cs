d34c4d26ac9e24ac9702099e44526ecfbd89f494
Cyberpixelgames <cyberpixelgames100@outlook.com>
2021-01-29 19:20:39 -0500

    Enemy Explosion

--- 7_(Argon Assault)/Assets/Scripts/Enemy.cs
+++ 7_(Argon Assault)/Assets/Scripts/Enemy.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject deathFX;
+    [SerializeField] Transform parent;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +17,8 @@ public class Enemy : MonoBehaviour
 
     void OnParticleCollision(GameObject other)
     {
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
         Destroy(gameObject);
     }
 }
