a42605ce7b01e7dcb491d2579c57331e09c0e84e
Cyberpixelgames <cyberpixelgames100@outlook.com>
2021-02-01 03:19:57 -0500

    Score Board

--- 7_(Argon Assault)/Assets/Scripts/Enemy.cs
+++ 7_(Argon Assault)/Assets/Scripts/Enemy.cs
@@ -7,9 +7,18 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathFX;
     [SerializeField] Transform parent;
+    [SerializeField] int ScorePerHit = 12;
+
+    ScoreBoard scoreBoard;
 
     // Start is called before the first frame update
     void Start()
+    {
+        AddBoxCollider();
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+
+    private void AddBoxCollider()
     {
         Collider boxCollider = gameObject.AddComponent<BoxCollider>();
         boxCollider.isTrigger = false;
@@ -17,6 +26,7 @@ public class Enemy : MonoBehaviour
 
     void OnParticleCollision(GameObject other)
     {
+        scoreBoard.ScoreHit(ScorePerHit);
         GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
         fx.transform.parent = parent;
         Destroy(gameObject);
